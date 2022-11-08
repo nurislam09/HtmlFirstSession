dde4070a75ddc698e1ba12fb7c726c935cf2edf0
nurislam09 <akinatorxxl@gmail.com>
2022-11-08 15:26:24 +0600

    Initial commit

---
+++ src/main/java/com/example/htmlfirstsession/HtmlFirstSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlfirstsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlFirstSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlFirstSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/htmlfirstsession/HtmlFirstSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlfirstsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlFirstSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
